63ea6a267eb78eafb80162b003d768ea90ba8cdb
Athanasios <tvoivodas@gmail.com>
2017-08-26 13:12:00 +0300

    implements validation of google's id_token

--- server.go
+++ server.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 var addr = flag.String("addr", ":9999", "The http server address")
@@ -21,6 +26,8 @@ func main() {
 	flag.Parse()
 	http.Handle("/", http.HandlerFunc(mainHandler))
 	http.Handle("/signin", http.HandlerFunc(signInHandler))
+	http.Handle("/idtoken", http.HandlerFunc(idTokenHandler))
+
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
@@ -29,11 +36,90 @@ func main() {
 }
 
 func mainHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
 	mainTempl.Execute(w, mainPageData)
 }
 
 func signInHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
 	signInTempl.Execute(w, signInPageData)
+}
+
+func idTokenHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	bodyAsString := string(body)
+
+	if strings.HasPrefix(bodyAsString, "idtoken=") == false {
+		http.Error(w, "invalid body request", http.StatusBadRequest)
+		return
+	}
+
+	idToken := bodyAsString[8:len(bodyAsString)]
+
+	valid, err := validateGoogleIDToken(idToken)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	} else if !valid {
+		http.Error(w, "invalid token", http.StatusUnauthorized)
+		return
+	}
+
+	log.Println("id token called")
+	fmt.Fprintf(w, "this seems to work")
+}
+
+func validateGoogleIDToken(token string) (valid bool, err error) {
+	valid = true
+
+	validateURI := fmt.Sprintf("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=%v", token)
+
+	resp, err := http.Get(validateURI)
+	if err != nil {
+		return false, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return false, errors.New("google return status code different then OK-200")
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	if err != nil {
+		return false, err
+	}
+
+	var data map[string]interface{}
+
+	if err := json.Unmarshal(body, &data); err != nil {
+		return false, err
+	}
+
+	if data["aud"].(string) != googleSignInClientID {
+		return false, errors.New("invalid audience")
+	}
+
+	return
 }
 
 func init() {
